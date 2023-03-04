6b3e1999acfa13ef983f0848b7eb5ecf9f1cfbdc
João Victor Cesa Andrade <82420106+Heartless-Caesar@users.noreply.github.com>
2023-03-03 22:10:04 -0300

    Add files via upload
    init

---
+++ 3.py
@@ -0,0 +1,20 @@
+seq_a = [1, 3, 5, 7, 9] # Sequencia de numeros ímpares
+seq_b = [2, 4, 8, 16, 32, 64, 128] # 2 elevado a n, em que n > 0
+seq_c = [0, 1, 4, 9, 16, 25, 36, 49, 66] # Soma de numeros ímpares a partir de 0 (0 + 1 = 1 + 3 = 4 + 5 = 9, ...)
+seq_d = [4, 16, 36, 64, 100] # Sequência em que cada elemento é n + n * 4, em que n é um número ímpar -> 4 = 1 * 4; 4 + 3 * 4 = 16; 16 + 5 *4 = 36
+seq_e = [1, 1, 2, 3, 5, 8] # Sequencia de Fibonacci
+seq_f = [2,10, 12, 16, 17, 18, 19, 200] # Números começam com a letra D
+
+# Próximo número ímpar -> 9
+# 2^7 = 128
+# 49 + 17 = 66
+# 64 + 9 * 4 = 100
+# 8 + 5 = 13
+# Duzentos 200
+
+print(seq_a)
+print(seq_b)
+print(seq_c)
+print(seq_d)
+print(seq_e)
+print(seq_f)
---
+++ 5.py
@@ -0,0 +1,4 @@
+string = input("Insira uma string\n")
+
+# Inicia da última string e vai até a primeira descrescendo o índice
+print(string[::-1])
